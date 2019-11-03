9526f2cc932c67c92ddd45639cc1a8e9aadc775c
kadjiah <kadijahwilson@pursuit.org>
2019-11-02 20:49:33 -0400

    frontend updated

--- server/client/index.js
+++ server/client/index.js
@@ -2,8 +2,11 @@ document.addEventListener('DOMContentLoaded', () =>{
     const getAllButton = document.querySelector('#getAll');
     getAllButton.addEventListener('click', loadPost);
 
+    const user = document.querySelector('#getUser');
+    user.addEventListener('click', getUserPost);
+
     const form = document.querySelector('#addPostForm');
-    form.addEventListener('submit', addPostForm);
+    form.addEventListener('submit', addPostFormSubmitted);
 });
 
 async function loadPost() {
@@ -19,3 +22,20 @@ async function loadPost() {
     })
 }
 
+async function getUserPost() {
+const userID = document.querySelector('#user_id').value
+
+const response = await axios.get(`http://localhost:4140/posts/${userID}`)
+// console.log(response.data.user[0])
+    const empPara = document.querySelector('#userInfo')
+    empPara.innerText = JSON.stringify(response.data.user);
+
+}
+
+async function addPostFormSubmitted(event){
+    event.preventDefault();    
+const poster_id = document.querySelector('#posterIdInput').value
+const body = document.querySelector('#bodyInput').value
+let response = await axios.post(`http://localhost:4140/posts/register`, {poster_id, body});
+loadPost()
+}
